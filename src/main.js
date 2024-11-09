91f3bc0005fbc544dbec1cdb0af2c0c28ee6bab3
Justin Navarro <info@justinnavarro.net>
2024-11-09 13:35:09 -0500

    add the rest of the assets to the main.js file

--- src/main.js
+++ src/main.js
@@ -25,3 +25,10 @@ k.loadSprite("motobug", "graphics/motobug.png", {
   },
 });
 k.loadFont("mania", "fonts/mania.ttf");
+
+k.loadSound("destroy", "sounds/Destroy.wav");
+k.loadSound("hurt", "sounds/Hurt.wav");
+k.loadSound("hyper-ring", "sounds/HyperRing.wav");
+k.loadSound("jump", "sounds/Jump.wav");
+k.loadSound("ring", "sounds/Ring.wav");
+k.loadSound("city", "sounds/city.mp3");
